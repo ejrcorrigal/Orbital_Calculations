a6be5c683d8fb4c53526396f62f8bb15656b2d32
Eric Corrigal <ejrcorrigal@gmail.com>
2023-03-25 09:46:20 -0600

    Added the SPG4 error dictionary

--- SGP4_Propogator.py
+++ SGP4_Propogator.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sgp4.api import Satrec
+from sgp4.api import SGP4_ERRORS
 
 class SGP4_Propogator:
     def __init__(self, TLE_S, TLE_T, start_julian_date, end_julian_date, julian_date_step):
@@ -11,13 +12,12 @@ class SGP4_Propogator:
         self.julian_date_step = julian_date_step
 
         self.julian_date_array = np.arange(start_julian_date, end_julian_date, julian_date_step)
-        print(len(self.julian_date_array))
         self.satellite = Satrec.twoline2rv(self.TLE_S, self.TLE_T)
         
     def propogate(self):
         julian_fraction = self.julian_date_array%1
-        print(self.julian_date_array)
         julian_day_array = self.julian_date_array.astype(int)
+        
         x_positions = np.zeros((len(julian_fraction)))
         y_positions = np.zeros((len(julian_fraction)))
         z_positions = np.zeros((len(julian_fraction)))
@@ -39,8 +39,13 @@ class SGP4_Propogator:
         return self.julian_date_array, x_positions, y_positions, z_positions, x_velocities, y_velocities, z_velocities
     def handle_sgp4_error(self, error):
         #TODO
-        print(error)
-        return error
+        output = False
+        try:
+            error_meaning = SGP4_ERRORS[error]
+            output = error_meaning
+        except:
+            pass
+        return output
 
     def format_orbital_vectors(self, vector):
         x_components = vector[:, 0]
@@ -54,11 +59,11 @@ class SGP4_Propogator:
         return x_components, y_components, z_components
 
 if __name__ == "__main__":
-    TLE_S = '1 23839U 96020A   23083.12882862  .00000079  00000+0  00000+0 0  9999'
-    TLE_T = '2 23839   8.1196  56.2378 0004719 335.9421 217.7025  0.99993763 98445'
-    start_julian_date = 230805
-    end_julian_date = 23087
-    julian_date_step = 0.1
+    TLE_S = '1 37846U 11060A   23083.05314641 -.00000107  00000+0  00000+0 0  9993'
+    TLE_T = '2 37846  57.0791  13.7465 0004137   4.9723 355.0256  1.70475952 70985'
+    start_julian_date = 23083
+    end_julian_date = 23084
+    julian_date_step = 0.001
     Propogator = SGP4_Propogator(TLE_S, TLE_T, start_julian_date, end_julian_date, julian_date_step)
     jd_array, x, y, z, dx, dy, dz = Propogator.propogate()
     radius = (x**2 + y**2 + x**2)**0.5
